cbe41833af62eefc634ad48ac3dd2ef844a1698f
Iwan van der Schoor <ivanders@cisco.com>
2016-06-17 15:10:43 -0700

    adds back link

--- web/js/main.js
+++ web/js/main.js
@@ -4,8 +4,9 @@ var gitUsers = angular.module('gitUsers',['ngRoute']),
 	BASE_API = "https://api.github.com";
 
 
-// Define the `PhoneListController` controller on the `phonecatApp` module
-
+/**
+ * configures the router to show different partials based on the route
+ */
 gitUsers.config(['$locationProvider', '$routeProvider',
 	function config($locationProvider, $routeProvider) {
 		$locationProvider.hashPrefix('!');
@@ -20,9 +21,10 @@ gitUsers.config(['$locationProvider', '$routeProvider',
 	}
 ]);
 
-
+/**
+ * Controller that stores the GIST data on GitHub and in sessionStorage
+ */
 gitUsers.controller('UserGistController', function UserGistController($scope, $routeParams, $http, $window){
-
 	// passing in the $window service instead of using sessionStorage global, so it can be easily tested / mocked out.
 
 	// sets the userId based on the route params
@@ -36,12 +38,15 @@ gitUsers.controller('UserGistController', function UserGistController($scope, $r
 		localGist = JSON.parse(localGist);  // since session storage stores strings only.. JSON.parse this
 
 		$scope.formData = {
-			description : localGist.description,
+			description : localGist.description || "",
 			content  : localGist.files["file1.txt"].content
 		};
 
 	}
 
+	/**
+	 * saves the Gist to GitHub, and in sessionStorage.
+	 */
 	$scope.saveGist = function saveGist(){
 
 		// data structure for GIT GISTS:
@@ -63,9 +68,11 @@ gitUsers.controller('UserGistController', function UserGistController($scope, $r
 
 			var gitResp = json.data;
 
-			// update the local version of the GIST content to store the gist id.
+			// update the local version of the GIST content in sessionStorage to store the gist id.
+			// intentionally using the get / setItem apis instead of direct access for testability.
+			var localCacheData = JSON.parse($window.sessionStorage.getItem($scope.userId));
+				localCacheData.gistId = gitResp.id;
 
-			var localCacheData = JSON.parse($window.sessionStorage.getItem($scope.userId)).gistId = gitResp.id;
 			$window.sessionStorage.setItem($scope.userId, JSON.stringify(localCacheData));
 
 			// on success, set success message
@@ -73,16 +80,18 @@ gitUsers.controller('UserGistController', function UserGistController($scope, $r
 
 		}, function failure(error){
 
+			// on failure, show error to user
 			$scope.message = "Gist data not saved to GitHub";
 
 		});
-
 	}
-
-
-
 });
 
+
+/**
+ * List controller that takes input from the query box to grab the list of users from GitHub
+ *
+ */
 gitUsers.controller('UserListController', function UserListController($scope, $http, $location){
 
 	// HTTP CALLS: https://api.github.com/users/iwanvdschoor
@@ -90,7 +99,7 @@ gitUsers.controller('UserListController', function UserListController($scope, $h
 
 	$scope.user = "";
 
-
+	// redirects to the edit url -> GistController
 	$scope.editUser = function editUser(user){
 		console.log(user);
 		var url = '/edituser/' + user;
@@ -99,6 +108,7 @@ gitUsers.controller('UserListController', function UserListController($scope, $h
 		$location.path(url);		// change the url to the user specific url, and let ngRouter handle it from there
 	};
 
+	// gets the data off of GitHub
 	$scope.getData = function getData(){
 
 		$http.jsonp(BASE_API + "/search/users?q=" + $scope.q + "&callback=JSON_CALLBACK")
